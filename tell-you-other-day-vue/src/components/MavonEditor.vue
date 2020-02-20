5ceddc585d1a42845fe37b701aac4464e23f8a47
Ivan-blade <2734319366@qq.com>
2020-02-20 23:26:51 +0800

    editor init

---
+++ tell-you-other-day-vue/src/components/MavonEditor.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="editor">
+        <mavon-editor 
+            :toolbarsFlag=decision
+            defaultOpen="edit"
+            placeholder=" "
+        ></mavon-editor>
+    </div>
+</template>
+<script>
+// Local Registration
+import { mavonEditor } from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+export default {
+    name: 'editor',
+    components: {
+        mavonEditor
+        // or 'mavon-editor': mavonEditor
+    },
+    data() {
+      return {
+        decision: false
+      }
+    },
+}
+</script>
+<style>
+#editor {
+    margin: auto;
+    width: 100%;
+}
+</style>
--- tell-you-other-day-vue/src/views/Home.vue
+++ tell-you-other-day-vue/src/views/Home.vue
@@ -1,16 +1,40 @@
 <template>
-  <v-card
-    class="my-5 mx-auto"
-    max-width="344"
-    outlined
-  >
-    the field of diary
-  </v-card>
+  <v-row justify="center">
+    <v-date-picker
+      v-model="picker"
+      color="blue lighten-1"
+      width="100%"
+      :events="arrayEvents"
+      event-color="green lighten-1"
+      :picker-date.sync="picker"
+    ></v-date-picker>
+    <mavon-editor></mavon-editor>
+  </v-row>
 </template>
 
 <script>
-
-export default {
-  name: 'Home'
-}
+  import mavonEditor from '../components/MavonEditor'
+  export default {
+    name: 'Home',
+    data () {
+      return {
+        picker: new Date().toISOString().substr(0, 10),
+        arrayEvents: null,
+        pickerDate: null,
+      }
+    },
+    components: {
+      mavonEditor
+    },
+    watch: {
+      picker (val) {
+        console.log(val)
+      }
+    }
+  }
 </script>
+<style lang="less" scoped>
+  .editor{
+    z-index: -1;
+  }
+</style>
